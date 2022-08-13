fe6dc52c8cd9efd37df9df5999f62218fe475ced
apanisile <apanisile@systemspecs.com.ng>
2022-08-13 05:49:51 +0100

    converting to if-else statments

--- main.go
+++ main.go
@@ -6,7 +6,21 @@ import (
 )
 
 // func userDetails(){
+// var firstName, lastName, email string
+// 	var userTickets int
 
+// 	fmt.Print("Please enter your first name: ")
+// 	fmt.Scan(&firstName)
+
+// 	fmt.Print("Please enter your last name: ")
+// 	fmt.Scan(&lastName)
+
+// 	fmt.Print("Enter your email address: ")
+// 	fmt.Scan(&email)
+
+// 	fmt.Print("How mamy tickets do you want to buy: ")
+// 	fmt.Scan(&userTickets)
+// 	fmt.Printf("\nThank you %v for booking %v tickets! You will recieve a confirmation in your email at %v\n", firstName, userTickets, email)
 // }
 
 func main() {
@@ -15,7 +29,8 @@ func main() {
 	remainingTickets := 50
 
 	fmt.Printf("Hello! Welcome to %v booking application!\n", conferenceName)
-	fmt.Print("Get your tickets here to attend\n")
+	fmt.Print("Get your tickets here to attend!")
+	fmt.Printf("\nThere are %v tickets available\n\n", remainingTickets)
 	bookings := []string{}
 
 	for {
@@ -35,20 +50,43 @@ func main() {
 
 		fmt.Print("How mamy tickets do you want to buy: ")
 		fmt.Scan(&userTickets)
-		fmt.Printf("\nThank you %v for booking %v tickets! You will recieve a confirmation in your email at %v\n", firstName, userTickets, email)
 
-		//admin-panel
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
-
-		fmt.Printf("\nThere are %v tickets and %v tickets available\n", conferenceTicket, remainingTickets)
-
-		firstNames := []string{}
-		for _, booking := range bookings {
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
+		if userTickets > conferenceTicket {
+			fmt.Print("\nYou cant book more tickets than available at this time!\n")
+			continue
 		}
 
-		fmt.Printf("\nThe booked users are %v\n", firstNames)
+		if userTickets <= remainingTickets {
+			//admin-panel
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("\nThank you %v for booking %v ticket(s)! You will recieve a confirmation in your email at %v\n", firstName, userTickets, email)
+			fmt.Printf("\nThere are %v ticket(s) available\n\n", remainingTickets)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+
+			fmt.Printf("\nThe booked users are %v\n", firstNames)
+			if remainingTickets == 0 {
+				//end program
+				fmt.Println("\nThe conference is booked out. Come again next year!")
+				break
+			}
+
+		} else {
+			fmt.Printf("We have only %v ticket(s) remaining \n", remainingTickets)
+		}
 	}
 }
+
+// func exit(){
+// 	break
+// }
+
+// func admin-panel(){
+// 	fmt.Printf("\nThe booked users are %v\n", firstNames)
+// }
